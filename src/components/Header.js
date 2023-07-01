6b349bb10610a69d328dcaa9b96a6e5056b81afa
akpierce1 <93303047+akpierce1@users.noreply.github.com>
2023-06-30 20:09:52 -0700

    initial project setup

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+    return (
+      <div>
+        <header>
+          <h1>Anna Pierce</h1>
+          <h2>web + brand design</h2>
+        </header>
+        <nav></nav>
+      </div>
+    );
+  }
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Main() {
+    return (
+      <div>
+        <main>
+            <p>Hi, I’m Anna! I am an aspiring web and graphic designer with the intent of creating stylish designs for small businesses! My focus is to help bring companies’ visions to life by blending soulful designs with purposeful solutions. The ambition of my brand is to prepare intentional and artistic website layouts and packaging designs for small brands who value stylish, yet purposeful design.</p>
+
+            <p>If you have a business in need of a brand redesign, or if you are starting a new business, I would be thrilled to connect with you to help your vision come to life!</p>
+        </main>
+      </div>
+    );
+  }
