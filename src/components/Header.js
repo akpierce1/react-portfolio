5627cae32f36db36e63e6253fe9e7aa06c8dbb87
akpierce1 <93303047+akpierce1@users.noreply.github.com>
2023-07-04 13:01:11 -0700

    Fix hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,39 +4,36 @@ import workspace from '../images/workspace.jpg';
 import '../styles.css';
 
 export default function Header() {
-    const [hamburgerIsOpen, setHamburgerIsOpen] = useState(false);
+  const [hamburgerIsOpen, setHamburgerIsOpen] = useState(false);
 
-    return (
-      <div>
-        <header>
+  return (
+    <div>
+      <header>
         <nav class="navbar navbar-expand-md navbar-light bg-light p-3">
-        <div class="container">
+          <div class="container">
 
-          <div class="logo">
-            <p class="fs-3">Anna Pierce</p>
-            <p class="fs-5 title">web + brand design</p>
-          </div>
+            <div class="logo">
+              <p class="fs-3">Anna Pierce</p>
+              <p class="fs-5 title">web + brand design</p>
+            </div>
 
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation" onClick={() => {
-            setHamburgerIsOpen(!hamburgerIsOpen);
-          }}>
-                <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div class="collapse navbar-collapse" id="navbarText">
-            <div class="navbar-nav ms-auto">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation" onClick={() => {
+                setHamburgerIsOpen(!hamburgerIsOpen);
+              }}>
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class={`collapse navbar-collapse ${hamburgerIsOpen ? 'show' : ''}`} id="navbarText">
               <div class="navbar-nav ms-auto">
-                    <a class="nav-item nav-link" href="#about-section">About</a>
-                    <a class="nav-item nav-link" href="#projects-section">Projects</a>
-                    <a class="nav-item nav-link" href="#skills-section">Skills</a>
-                    <a class="nav-item nav-link" href="#experience-section">Experience</a>
-                </div>
+                  <a class="nav-item nav-link" href="#about-section">About</a>
+                  <a class="nav-item nav-link" href="#projects-section">Projects</a>
+                  <a class="nav-item nav-link" href="#skills-section">Skills</a>
+                  <a class="nav-item nav-link" href="#experience-section">Experience</a>
               </div>
             </div>
           </div>
-      </nav>
+        </nav>
       </header>
       <img src={workspace} class="img-fluid hero-img"/>
-      </div>
-    );
-  }
+    </div>
+  );
+}
