15f2042220dd1a1b915626ae2397b131ab0d93f5
akpierce1 <93303047+akpierce1@users.noreply.github.com>
2023-07-05 15:32:28 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-// import img1 from './images/workspace.jpg'
 
 export default function App() {
   return (
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,24 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.css';
-import '../styles.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles.css";
 
 export default function Footer() {
-    return (
-      <div>
-        <footer>
+  return (
+    <div>
+      <footer>
         <h5>Let's get in touch!</h5>
 
-        <a href="https://github.com/akpierce1" target={"_blank"}>GitHub</a>
-        <a href="https://www.linkedin.com/in/annakpierce/" target={"_blank"}>LinkedIn</a>
+        <a href="https://github.com/akpierce1" target={"_blank"}>
+          GitHub
+        </a>
+        <a href="https://www.linkedin.com/in/annakpierce/" target={"_blank"}>
+          LinkedIn
+        </a>
 
-        <p class="copyright">Copyright 2023 Anna Pierce. All rights reserved.</p>
-        </footer>
-      </div>
-    );
-  }
+        <p class="copyright">
+          Copyright 2023 Anna Pierce. All rights reserved.
+        </p>
+      </footer>
+    </div>
+  );
+}
